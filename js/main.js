b004d7d38a21746b5e26648b719e74cfda092303
Konstantin <kandruk@ya.ru>
2020-06-04 22:52:19 +0300

    module3-task2 try 2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
 'use strict';
-
-var createPins = function (arr) {
-  var PIN_OFFSET_X = -25;
-  var PIN_OFFSET_Y = -70;
-  var pinTemplate = document.querySelector('#pin')
+var TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var ADDRES_ADD_AVATAR = [1, 2, 3, 4, 5, 6, 7, 8];
+var PIN_OFFSET_X = -25;
+var PIN_OFFSET_Y = -70;
+var NUMBER_OF_PINS = 8;
+var MIN_VALUE_X = 25;
+var MIN_VALUE_Y = 130;
+var MAX_VALUE_Y = 630;
+var pinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
-  var fragment = document.createDocumentFragment();
+var mapPins = document.querySelector('.map__pins');
 
-  for (var i = 0; i < arr.length; i++) {
-    var pinElement = pinTemplate.cloneNode(true);
-    pinElement.style = 'left:' + (arr[i].location.x + PIN_OFFSET_X) + 'px; ' + 'top:' + (arr[i].location.y + PIN_OFFSET_Y) + 'px;';
-    pinElement.firstElementChild.src = arr[i].author.avatar;
-    pinElement.firstElementChild.alt = arr[i].offer.title;
-    fragment.appendChild(pinElement);
-  }
+var createPin = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
+  var pinImg = pinElement.querySelector('img');
+  pinElement.style = 'left:' + (pin.location.x + PIN_OFFSET_X) + 'px; ' + 'top:' + (pin.location.y + PIN_OFFSET_Y) + 'px;';
+  pinImg.src = pin.author.avatar;
+  pinImg.alt = pin.offer.title;
+  return pinElement;
+};
+
+var createPins = function (arr) {
+  var fragment = document.createDocumentFragment();
+  arr.forEach(function (item) {
+    fragment.appendChild(createPin(item));
+  });
   return fragment;
 };
 
 var renderPins = function (pins) {
-  var mapPins = document.querySelector('.map__pins');
   mapPins.appendChild(pins);
 };
 
-var mapActivate = function () {
+var activateMap = function () {
   var map = document.querySelector('.map');
   map.classList.remove('map--faded');
 };
@@ -48,23 +61,31 @@ var getRandomElementOfArray = function (arr) {
   return arr[rand];
 };
 
-var getDemoData = function () {
-  var TIMES = ['12:00', '13:00', '14:00'];
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-  var data = [];
-  var mapPins = document.querySelector('.map__pins');
+var shuffle = function (arr) {
+  var copy = [];
+  var n = arr.length;
+  var i;
 
-  for (var i = 1; i <= 8; i++) {
+  while (n) {
+    i = Math.floor(Math.random() * n--);
+    copy.push(arr.splice(i, 1)[0]);
+  }
+  return copy;
+};
+
+var getDemoData = function () {
+  var data = [];
+  var avatars = shuffle(ADDRES_ADD_AVATAR);
+
+  for (var i = 0; i < NUMBER_OF_PINS; i++) {
     var item = {
       author: {
-        avatar: 'img/avatars/user0' + i + '.png'
+        avatar: 'img/avatars/user0' + avatars[i] + '.png'
       },
       offer: {
         title: 'заголовок предложения ' + i,
         address: '600, 350',
-        price: (i * 345) + ' $',
+        price: 345 + (i * 345) + ' $',
         type: getRandomElementOfArray(TYPES),
         rooms: getRandomInteger(1, 4),
         guests: getRandomInteger(1, 6),
@@ -75,8 +96,8 @@ var getDemoData = function () {
         photos: getArrayRandomLength(PHOTOS)
       },
       location: {
-        x: getRandomInteger(25, mapPins.offsetWidth - 25),
-        y: getRandomInteger(130, 630)
+        x: getRandomInteger(MIN_VALUE_X, mapPins.offsetWidth - 25),
+        y: getRandomInteger(MIN_VALUE_Y, MAX_VALUE_Y)
       }
     };
     data.push(item);
@@ -86,5 +107,5 @@ var getDemoData = function () {
 
 var data = getDemoData();
 var pins = createPins(data);
-mapActivate();
+activateMap();
 renderPins(pins);
