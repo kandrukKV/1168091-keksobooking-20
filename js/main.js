1ca8767945b166c04fb1c3a73e7996cc0e768801
Konstantin <kandruk@ya.ru>
2020-06-16 11:27:23 +0300

    module5-task3

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
 (function () {
-  var GENERAL_PIN_OFFSET_Y = 22;
   var KEY_ENTER = 'Enter';
-  var MIN_VALUE_Y = 130;
-  var MAX_VALUE_Y = 630;
   var mainPin = document.querySelector('.map__pin--main');
   var data = window.data;
 
@@ -22,101 +19,20 @@
     }
   };
 
-  var getCoordinatesMainPin = function () {
-    return {
-      x: parseInt(mainPin.style.left, 10) + Math.ceil(mainPin.offsetWidth / 2),
-      y: parseInt(mainPin.style.top, 10) + mainPin.offsetHeight + GENERAL_PIN_OFFSET_Y
-    };
-  };
-
-  var getCentralCoordinatesMainPin = function () {
-    return {
-      x: parseInt(mainPin.style.left, 10) + Math.ceil(mainPin.offsetWidth / 2),
-      y: parseInt(mainPin.style.top, 10) + Math.ceil(mainPin.offsetWidth / 2)
-    };
-  };
-
   var activatePage = function () {
-    var coordinates = getCoordinatesMainPin();
+    var coordinates = window.move.getCoordinatesElement(mainPin);
     window.form.setAddress(coordinates);
     window.map.activate(data);
     window.form.activate();
-    moveMainPin();
+    window.move.activate(mainPin);
     mainPin.removeEventListener('mousedown', onMainPinMouseDown);
     mainPin.removeEventListener('keydown', onMainPinEnterPress);
   };
 
-  var coordinates = getCentralCoordinatesMainPin();
+  var coordinates = window.move.getCentralCoordinatesElement(mainPin);
   window.form.setAddress(coordinates);
   window.form.disable();
   mainPin.addEventListener('mousedown', onMainPinMouseDown);
   mainPin.addEventListener('keydown', onMainPinEnterPress);
 
-  // ТУТ НАРАБОТКИ ДЛЯ ВТОРОЙ ЧАСТи ДЗ move mainPin
-
-  var moveMainPin = function () {
-    var mapPins = document.querySelector('.map__pins');
-    mainPin.addEventListener('mousedown', function (evt) {
-      evt.preventDefault();
-      var mainPinMoveLimits = {
-        top: MIN_VALUE_Y - mainPin.offsetHeight - GENERAL_PIN_OFFSET_Y,
-        bottom: MAX_VALUE_Y - mainPin.offsetHeight - GENERAL_PIN_OFFSET_Y,
-        left: mapPins.offsetLeft - Math.ceil(mainPin.offsetWidth / 2),
-        right: mapPins.offsetWidth - Math.ceil(mainPin.offsetWidth / 2)
-      };
-
-      var startPosition = {
-        x: evt.clientX,
-        y: evt.clientY
-      };
-
-      var renderMainPin = function (position) {
-        mainPin.style.top = position.y + 'px';
-        mainPin.style.left = position.x + 'px';
-      };
-
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-
-        var shift = {
-          x: startPosition.x - moveEvt.clientX,
-          y: startPosition.y - moveEvt.clientY
-        };
-
-        startPosition = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-
-        var pinPosition = {
-          x: mainPin.offsetLeft - shift.x,
-          y: mainPin.offsetTop - shift.y
-        };
-
-        if (pinPosition.x < mainPinMoveLimits.left) {
-          pinPosition.x = mainPinMoveLimits.left;
-        } else if (pinPosition.x > mainPinMoveLimits.right) {
-          pinPosition.x = mainPinMoveLimits.right;
-        }
-
-        if (pinPosition.y < mainPinMoveLimits.top) {
-          pinPosition.y = mainPinMoveLimits.top;
-        } else if (pinPosition.y > mainPinMoveLimits.bottom) {
-          pinPosition.y = mainPinMoveLimits.bottom;
-        }
-
-        renderMainPin(pinPosition);
-        window.form.setAddress(getCoordinatesMainPin());
-      };
-
-      var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-      };
-
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-    });
-  };
 })();
